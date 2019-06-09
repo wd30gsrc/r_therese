1c6547627938c2b31edc93f4693fec4457e24f66
wd30gsrc <gd28.c.tateno@gmail.com>
2019-06-09 17:26:36 +0900

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,28 @@
-// gulpプラグインの読み込み
-var gulp = require('gulp');
-var notify = require("gulp-notify"); // エラー時の通知
-var plumber = require('gulp-plumber'); // エラーでもファイル監視(watch)を続ける
-var sass = require("gulp-sass"); //SassをCSSに
-var autoprefixer = require("gulp-autoprefixer"); //Sassにベンダープレフィックスをつける
-var uglify = require('gulp-uglify'); //JSの圧縮
-var plumber = require("gulp-plumber"); //エラーハンドリング
-var pug = require('gulp-pug');
+// gulpプラグインを読み込みます
+const { src, dest, watch } = require("gulp");
+// Sassをコンパイルするプラグインを読み込みます
+const sass = require("gulp-sass");
 
-//scssにベンダープレフィックスをつけて圧縮してコンパイル
-gulp.task('scss', function() {
-    gulp.src('src/sass/*.scss')
-        .pipe(plumber())
-        .pipe(autoprefixer({ // ベンダープレフィックスの付与
-            browsers: ['last 2 version', 'ie >= 10','iOS >= 11', 'Android >= 4.4'], // (ベンダープレフィックスを付与する)対応ブラウザの指定
-            cascade: false // 整形しない
-        }))
-        .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('dest/css')); // cssディレクトリに書き出す
-});
+/**
+ * Sassをコンパイルするタスクです
+ */
+const compileSass = () =>
+  // style.scssファイルを取得
+  src("css/*.scss")
+    // Sassのコンパイルを実行
+    .pipe(
+      // コンパイル後のCSSを展開
+      sass({
+        outputStyle: "expanded"
+      })
+    )
+    // cssフォルダー以下に保存
+    .pipe(dest("dest/css"));
 
-//jsを圧縮
-gulp.task('js', function() {
-    //実行するファイル
-    gulp.src('src/js/*.js')
-        //pipeでつなぐ
-        .pipe(plumber())//エラーハンドリング
-        .pipe(uglify())// 圧縮
-        .pipe(gulp.dest('dest/js/min'));//圧縮したファイルを出力
-});
+/**
+ * Sassファイルを監視し、変更があったらSassを変換します
+ */
+const watchSassFiles = () => watch("css/*.scss", compileSass);
 
-//Pug
-gulp.task('pug', () => {
-    return gulp.src('src/pug/*.pug')
-        .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest('dest'));
-});
-
-
-
-// 自動監視化
-gulp.task('watch', function(){
-    gulp.watch('src/scss/*.scss', ['scss']);
-    gulp.watch('src/js/*.js', ['js']);
-    gulp.watch('src/pug/*.pug', ['pug']);
-});
-
-gulp.task('default', ['watch']);
+// npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
+exports.default = watchSassFiles;

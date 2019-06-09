89f593fce59d5cb5a98ac827a4842bee61e2d2aa
wd30gsrc <gd28.c.tateno@gmail.com>
2019-06-09 19:38:54 +0900

    gulpfile.js 変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,35 @@
 // gulpプラグインを読み込みます
-const { src, dest, watch } = require("gulp");
-// Sassをコンパイルするプラグインを読み込みます
+const { src, dest, watch, parallel, series} = require("gulp");
 const sass = require("gulp-sass");
+const webserver = require("gulp-webserver");
 
-/**
- * Sassをコンパイルするタスクです
- */
-const compileSass = () =>
-  // style.scssファイルを取得
-  src("src/sass/*.scss")
-    // Sassのコンパイルを実行
-    .pipe(
-      // コンパイル後のCSSを展開
-      sass({
-        outputStyle: "expanded"
-      })
-    )
-    // cssフォルダー以下に保存
-    .pipe(dest("dest/css/"));
+
+function compileSass(){
+  return src('src/sass/*.scss')
+  .pipe(
+    sass({
+      outputStyle: "expanded"
+    })
+  )
+  .pipe(dest('dest/css'))
+}
+
+function webServer(){
+  return src('dest/')
+  .pipe(webserver({
+    host: 'localhost',
+    port: 8000,
+    livereload: true,
+    open: true
+  }))
+}
 
 /**
  * Sassファイルを監視し、変更があったらSassを変換します
  */
-const watchSassFiles = () => watch("src/sass/*.scss", compileSass);
+function watchSassFiles(){
+  watch("src/sass/*.scss", compileSass);
+}
 
 // npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
-exports.default = watchSassFiles;
+exports.default = series(watchSassFiles, webServer);
